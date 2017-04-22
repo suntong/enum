d5c60ea600fdac86662c47806595a130b9c6e51e
Tong Sun <suntong@cpan.org>
2017-04-22 17:41:42 -0400

    - [*] add comments, to fix for golint

--- Enum.go
+++ Enum.go
@@ -9,17 +9,21 @@ package enum
 
 var enums []string
 
+// The Enum is the type to hold enumerables
 type Enum int
 
+// String turns Enum to its string form
 func (e Enum) String() string {
 	return enums[int(e)]
 }
 
+// Ciota converts string to enumerable, similar to Go's iota
 func Ciota(s string) Enum {
 	enums = append(enums, s)
 	return Enum(len(enums) - 1)
 }
 
+// Get lookup the given string for interal enumerable
 func Get(s string) (Enum, bool) {
 	for ii, vv := range enums {
 		if vv == s {
